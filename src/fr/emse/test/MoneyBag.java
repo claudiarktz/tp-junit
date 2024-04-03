078488e46826a92ee0d339b6d27326fc95f603d2
Nekena <claudiarktz@gmail.com>
2024-04-03 21:16:05 +0200

    Changement de code pour la classe MoneyBag

--- src/fr/emse/test/MoneyBag.java
+++ src/fr/emse/test/MoneyBag.java
@@ -7,14 +7,29 @@ import java.util.Objects;
 public class MoneyBag {
 	private Vector<Money> fMonies = new Vector<Money>();
 	
+	public MoneyBag(Money m) {
+		appendMoney(m);
+	}
+	
+	public MoneyBag(Money m, MoneyBag mb) {
+		appendMoney(m);
+		for (Money money : mb.fMonies) {
+			appendMoney(money);
+		}
+	}
+	
+	
+	
 	MoneyBag(Money m1, Money m2) {
 		appendMoney(m1);
 		appendMoney(m2);
+
 	}
-	
+
 	MoneyBag(Money bag[]) {
-		for (int i = 0; i < bag.length; i++)
+		for (int i = 0; i < bag.length; i++) {
 			appendMoney(bag[i]);
+		}
 	}
 	
 	private void appendMoney(Money m) {
@@ -22,18 +37,17 @@ public class MoneyBag {
 			fMonies.add(m);
 		} else {
 			int i = 0;
-			while ((i < fMonies.size())
-					&& (!(fMonies.get(i).currency().equals(m.currency()))))
+			while ((i < fMonies.size()) && (!(fMonies.get(i).currency().equals(m.currency()))))
 				i++;
 			if (i >= fMonies.size()) {
 				fMonies.add(m);
 			} else {
-				fMonies.set(i, new Money(fMonies.get(i).amount() +
-						m.amount(),
-						m.currency()));
+				fMonies.set(i, new Money(fMonies.get(i).amount() + m.amount(), m.currency()));
 			}
 		}
 	}
+	
+	
 	@Override
 	public boolean equals(Object compare) {
 		if (this == compare) return true;
